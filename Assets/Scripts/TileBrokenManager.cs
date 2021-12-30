5621440490e9dbeda142e9c6ee19128924b20bea
adimakopoulos <adimakopoulosPs@users.noreply.github.com>
2021-12-30 18:23:57 +0200

    Added Models

---
+++ Assets/Scripts/TileBrokenManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TileBrokenManager : MonoBehaviour
+{
+    public Rigidbody[] BrokenPieces ;
+    public float TimeToLive;
+    void Start()
+    {
+        foreach (var rb in BrokenPieces)
+        {
+            rb.AddForce(new Vector3(-1 * Random.Range(0f, 1f), 1, 1 * Random.Range(0f, 1f)) * 300);
+            rb.AddTorque(new Vector3(-1 * Random.Range(0f, 1f), 1, 1 * Random.Range(0f, 1f)) , ForceMode.Impulse);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
